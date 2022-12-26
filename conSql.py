b411d7d861f17c9f0a0ccc076402bda9d74deb97
gioliano Gomez <gioliano.gomez@gmail.com>
2022-12-26 08:18:37 -0500

    nuevo archivo para conectarme en sql

---
+++ conSql.py
@@ -0,0 +1,21 @@
+#importamos la libreria para conectarnos al sql
+#pip install pyodbc
+import pyodbc
+#variable para la conexion de la base de datos
+#colocar el usuario administrador de la base d edatos
+server='192.168.0.131'
+usuario="admin"
+clave="123456"
+#llevanos a cabo la conexion primera fomma
+#conexion= pyodbc.connect(driver='{SQL server}',host=server)
+#segunda forma
+conexion=pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';UID='+usuario+';PWD='+clave,autocommit=True)
+print("conexión exitosa")
+
+#creamos cursor para almacenar en memoria
+cursor=conexion.cursor()
+#crear un base de datos desde python
+cursor.execute("CREATE DATABASE DB_prueba01")
+
+#cerrar conexion
+conexion.close()
