49c56826502c31ba66c9aa95da2d9acf6777c677
gioliano Gomez <gioliano.gomez@gmail.com>
2022-12-26 13:33:44 -0500

    nuevo modulo de select

---
+++ conSqlselect.py
@@ -0,0 +1,15 @@
+import pyodbc ##pip install pyodbc
+import pandas as pd #pip install pandas
+server='192.168.0.131'
+usuario="admin"
+clave="123456"
+
+db_name="db_PRUEBAA"
+#segunda forma
+#conexion= pyodbc.connect(driver='{SQL server}',host=server,database=db_name)
+conexion=pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+db_name+';UID='+usuario+';PWD='+clave,autocommit=True)
+print("conexion exitosa")
+#leer la informacion de la base de datos
+ciudad=pd.read_sql("SELECT * FROM ciudad",conexion)
+#cerrar conexion
+conexion.close()
